663b72b2e3dd21c30cdb82041abc76836a2f129a
daviolvr <olidavi031130@gmail.com>
2025-05-23 18:43:44 -0300

    feat: password hasher for users

--- core/settings.py
+++ core/settings.py
@@ -131,3 +131,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+]

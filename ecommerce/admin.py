b6e21aa25cfc96cb50554ce1d936e9578e0441a3
Davi de Oliveira <olidavi031130@gmail.com>
2025-05-27 10:13:38 -0300

    feat: basic admin setup

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product, Category, Order, OrderItem, Review
 
-# Register your models here.
+admin.site.site_header = 'E-Commerce Admin'
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Review)

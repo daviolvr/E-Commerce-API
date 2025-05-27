2dfcc7fc901727695c5732e42c644efdd9d6c531
Davi de Oliveira <olidavi031130@gmail.com>
2025-05-27 10:34:45 -0300

    feat: basic serializer

---
+++ ecommerce/serializers.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Product, Category, Order, OrderItem, Review
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'

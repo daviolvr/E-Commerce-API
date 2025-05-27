e2b022e7487c5090faa3b182da5ca8f690deb288
Davi de Oliveira <olidavi031130@gmail.com>
2025-05-27 10:37:46 -0300

    feat: user serializer

--- ecommerce/serializers.py
+++ ecommerce/serializers.py
@@ -2,6 +2,13 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Product, Category, Order, OrderItem, Review
 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product

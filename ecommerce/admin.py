50be3223ab30f4e4f3dbf8ca84c47b670f373a89
Davi de Oliveira <olidavi031130@gmail.com>
2025-06-01 20:44:55 -0300

    feat: new admin registers for Cart, CartProduct, Shipping, Payment

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category, Order, OrderProduct, Review, ProductCategory
+from .models import Product, Category, Order, OrderProduct, Review, Cart, CartProduct, Shipping, Payment
 
 admin.site.site_header = 'E-Commerce Admin'
 admin.site.register(Product)
@@ -7,4 +7,7 @@ admin.site.register(Category)
 admin.site.register(Order)
 admin.site.register(OrderProduct)
 admin.site.register(Review)
-admin.site.register(ProductCategory)
+admin.site.register(Cart)
+admin.site.register(CartProduct)
+admin.site.register(Shipping)
+admin.site.register(Payment)

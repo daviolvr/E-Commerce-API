46add897376a57c9384ce867cdbd2055da8fe834
Davi de Oliveira <olidavi031130@gmail.com>
2025-06-01 20:45:57 -0300

    feat: new views for Cart, CartProduct, Shipping, Payments

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,13 +3,15 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from .models import Product, Category, Order, OrderProduct, Review, ProductCategory
+from .models import Product, Category, Order, OrderProduct, Review, Cart, CartProduct, Shipping, Payment
 from .serializers import (UserSerializer, ProductSerializer, CategorySerializer, OrderSerializer, 
-                          OrderProductSerializer, ReviewSerializer, ProductCategorySerializer)
+                          OrderProductSerializer, ReviewSerializer, CartSerializer, CartProductSerializer,
+                          ShippingSerializer, PaymentSerializer)
 from drf_spectacular.utils import extend_schema
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import (UserFilter, ProductFilter, CategoryFilter, OrderFilter, 
-                      OrderProductFilter, ReviewFilter, ProductCategoryFilter)
+                      OrderProductFilter, ReviewFilter, CartFilter, CartProductFilter,
+                      ShippingFilter, PaymentFilter)
 from rest_framework.permissions import IsAdminUser
 from .permissions import IsAdminOrReadOnly
 
@@ -86,12 +88,48 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 
 @extend_schema(
-    summary="Operações relacionadas aos ProductCategory"
+    summary="Operações relacionadas ao Cart"
 )
-class ProductCategoryViewSet(viewsets.ModelViewSet):
-    queryset = ProductCategory.objects.all()
-    serializer_class = ProductCategorySerializer
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
     filter_backends = [DjangoFilterBackend]
-    filterset_class = ProductCategoryFilter
+    filterset_class = CartFilter
+    permission_classes = [IsAdminUser]
+
+
+@extend_schema(
+    summary="Operações relacionadas aos CartProduct"
+)
+class CartProductViewSet(viewsets.ModelViewSet):
+    queryset = CartProduct.objects.all()
+    serializer_class = CartProductSerializer
+    http_method_names = ['get', 'post', 'delete']
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = CartProductFilter
+    permission_classes = [IsAdminUser]
+
+
+@extend_schema(
+    summary="Operações relacionadas ao Shipping"
+)
+class ShippingViewSet(viewsets.ModelViewSet):
+    queryset = Shipping.objects.all()
+    serializer_class = ShippingSerializer
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ShippingFilter
+    permission_classes = [IsAdminUser]
+
+
+@extend_schema(
+    summary="Operações relacionadas ao Payment"
+)
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = PaymentFilter
     permission_classes = [IsAdminUser]

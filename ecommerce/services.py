059c9fcd4ce6208eef44c4111215e8c959a9b486
Davi de Oliveira <olidavi031130@gmail.com>
2025-06-02 10:56:14 -0300

    fix: ensuring tracking_number is always new

--- ecommerce/services.py
+++ ecommerce/services.py
@@ -1,14 +1,21 @@
 import random
 import string
-from .models import Shipping
+import uuid
+from .models import Shipping, Payment
 
 def generate_tracking_number() -> str:
     prefix = 'EC'
-    num = ''.join(random.choices(string.digits, k=9))
     sufix = 'BR'
-    tracking_number = f"{prefix}{num}{sufix}"
-
-    if Shipping.objects.filter(tracking_number=tracking_number).exists():
-        return "Invalid tracking number: already exists"
     
-    return tracking_number
+    while True:
+        num = ''.join(random.choices(string.digits, k=9))
+        tracking_number = f"{prefix}{num}{sufix}"
+        if not Shipping.objects.filter(tracking_number=tracking_number).exists():
+            return tracking_number
+
+
+def generate_transaction_id() -> str:
+    while True:
+        transaction_id = str(uuid.uuid4()).replace('-', '')[:12].upper()
+        if not Payment.objects.filter(transaction_id=transaction_id).exists():
+            return transaction_id

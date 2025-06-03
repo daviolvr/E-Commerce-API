a37862a2685dedf293f9c508ec11f49c430beb03
Davi de Oliveira <olidavi031130@gmail.com>
2025-06-03 15:12:06 -0300

    fix: access token lifetime misspell corrected

--- core/settings.py
+++ core/settings.py
@@ -161,6 +161,6 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLEJWT = {
-    "ACESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
     # "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }

179f174252be3c11e64253b3f04ed69a08d981bd
Davi de Oliveira <olidavi031130@gmail.com>
2025-08-04 22:35:11 -0300

    refactor: separating apps in settings.py

--- core/settings.py
+++ core/settings.py
@@ -37,18 +37,27 @@ ALLOWED_HOSTS = json.loads(os.environ.get("ALLOWED_HOSTS", "[]"))
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+]
+
+THIRD_PARTY_APPS = [
     "rest_framework",
     "drf_spectacular",
     "rest_framework_simplejwt",
+]
+
+LOCAL_APPS = [
     "ecommerce",
 ]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
